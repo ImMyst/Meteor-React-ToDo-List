a88a62aade2a6b40f591f6d40a4041ee78f50479
Titouan <titouan.galvani@laposte.net>
2018-07-12 23:23:05 +0200

    Import API tasks.js in main.js :hammer:

--- client/main.js
+++ client/main.js
@@ -3,8 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import './main.html';
 import { render } from 'react-dom';
 
-
-import App from '../imports/ui/App.js';
+import App from '../imports/ui/App';
+import '../imports/api/tasks'
 
 Meteor.startup(() => {
     render(<App />, document.getElementById('render-target'));
